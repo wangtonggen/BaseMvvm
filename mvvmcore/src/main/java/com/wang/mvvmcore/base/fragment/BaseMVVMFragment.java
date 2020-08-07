07308f971d5c60850fabaa5fec1edbfe06a3a0de
王统根 <815786482@qq.com>
2020-08-07 17:25:35 +0800

    1.rootView复用 2.LazyLoadFragment包内可继承

--- mvvmcore/src/main/java/com/wang/mvvmcore/base/fragment/BaseMVVMFragment.java
+++ mvvmcore/src/main/java/com/wang/mvvmcore/base/fragment/BaseMVVMFragment.java
@@ -27,8 +27,9 @@ public abstract class BaseMVVMFragment<B extends ViewDataBinding, VM extends Bas
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        LogUtils.logE("create=" + TAG);
-        binding = DataBindingUtil.inflate(inflater, getLayoutRes(), container, false);
+        if (binding == null){
+            binding = DataBindingUtil.inflate(inflater, getLayoutRes(), container, false);
+        }
         initView(savedInstanceState, binding.getRoot());
         return binding.getRoot();
     }
--- mvvmcore/src/main/java/com/wang/mvvmcore/base/fragment/BaseNoMVVMFragment.java
+++ mvvmcore/src/main/java/com/wang/mvvmcore/base/fragment/BaseNoMVVMFragment.java
@@ -13,7 +13,7 @@ import butterknife.ButterKnife;
 /**
  * author: wtg
  * date:2020/3/21 0021
- * desc: 不适用mvvm的fragment的基类
+ * desc: 不适用binding的fragment的基类
  */
 public abstract class BaseNoMVVMFragment extends BaseLazyLoadFragment {
     protected View rootView;
@@ -21,9 +21,11 @@ public abstract class BaseNoMVVMFragment extends BaseLazyLoadFragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        rootView = LayoutInflater.from(mContext).inflate(getLayoutRes(), container, false);
+        if (rootView == null){
+            rootView = LayoutInflater.from(mContext).inflate(getLayoutRes(), container, false);
+        }
         ButterKnife.bind(rootView);
         initView(savedInstanceState, rootView);
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return rootView;
     }
 }
